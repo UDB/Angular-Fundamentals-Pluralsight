5b72135669f5db48573d030c3e03c9d040a62582
Udit Bhardwaj <itsmeudit@gmail.com>
2019-04-29 10:55:48 +0530

    Wrapping Third Party Services

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service';
 
 @Component({
   selector: "events-list",
@@ -10,7 +11,8 @@ import { EventService } from './shared/event.service';
       <div class="row">
         <div class="col-md-5" *ngFor="let event of events">
           <event-thumbnail
-            [event]="event">
+            [event]="event"
+            (click)="handleThumbnailClick(event.name)">
           </event-thumbnail>
         </div>
       </div>
@@ -19,12 +21,16 @@ import { EventService } from './shared/event.service';
 })
 export class EventsListComponent implements OnInit {
   events: any[];
-  constructor(private eventService: EventService) {
+  constructor(private eventService: EventService, private toastr: ToastrService) {
   }
 
   ngOnInit(): void {
     // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     // Add 'implements OnInit' to the class.
     this.events = this.eventService.getEvents();
+  }
+
+  handleThumbnailClick(eventName) {
+    this.toastr.success(eventName);
   }
 }
