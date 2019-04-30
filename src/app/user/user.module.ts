5955b74d165370e5bdb1a3f04210bba3f2eb1e77
Udit Bhardwaj <itsmeudit@gmail.com>
2019-04-30 10:48:22 +0530

    Using the Data from Your Template-based Form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
 import { ProfileComponent } from './profile.component';
 import { userRoutes } from './user.routes';
 import { LoginComponent } from './login.component';
@@ -9,6 +12,7 @@ import { LoginComponent } from './login.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(userRoutes)
   ],
   declarations: [
